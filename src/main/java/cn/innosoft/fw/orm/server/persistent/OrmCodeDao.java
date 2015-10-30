8b4533267a1c1f2831b64827e4d71dd52b0ecf3f
王民泽 <wangmz@9tuo.com>
2015-10-30 16:20:21 +0800

    代码模块更新(不完善)

--- src/main/java/cn/innosoft/fw/orm/server/persistent/OrmCodeDao.java
+++ src/main/java/cn/innosoft/fw/orm/server/persistent/OrmCodeDao.java
@@ -2,6 +2,8 @@ package cn.innosoft.fw.orm.server.persistent;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+
 import cn.innosoft.fw.biz.core.persistent.BaseDao;
 import cn.innosoft.fw.orm.server.model.OrmCode;
 
@@ -10,4 +12,12 @@ public interface OrmCodeDao extends BaseDao<OrmCode, String> {
 	public List<OrmCode> findByParentCodeId(String parentCodeId);
 
 	public Long deleteBySystemId(String systemId);
+	
+	public List<OrmCode> findBySystemId(String systemId);
+	
+	public List<OrmCode> findBySystemIdAndParentCodeId(String systemId,String parentCodeId);
+	
+	@Query(value = "select * from ORM_CODE where SYSTEM_ID = ?1 and PARENT_CODE_ID = 'ROOT' ",nativeQuery=true)
+	public List<OrmCode> getRootCodeBySystemId(String systemId);
+	
 }
